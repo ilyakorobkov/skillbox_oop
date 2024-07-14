fc5cd71e642f3c02b19aae1ae730b6416eede6c2
ilyakorobkov <112753624+ilyakorobkov@users.noreply.github.com>
2024-07-14 10:56:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,129 @@
 #include <iostream>
-#include "Car.h"
-#include "Lorry.h"
+#include <vector>
+
+class Matrix {
+private:
+    std::vector<std::vector<int>> data;
+    int rows, cols;
+
+public:
+    // Конструктор
+    Matrix(int r, int c) : rows(r), cols(c) {
+        data.resize(rows, std::vector<int>(cols, 0));
+    }
+
+    // Перегрузка оператора сложения
+    Matrix operator+(const Matrix &other) const {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = this->data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    // Перегрузка оператора вычитания
+    Matrix operator-(const Matrix &other) const {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = this->data[i][j] - other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    // Перегрузка оператора умножения
+    Matrix operator*(const Matrix &other) const {
+        Matrix result(rows, other.cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < other.cols; ++j) {
+                result.data[i][j] = 0;
+                for (int k = 0; k < cols; ++k) {
+                    result.data[i][j] += this->data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    // Перегрузка оператора +=
+    Matrix& operator+=(const Matrix &other) {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                this->data[i][j] += other.data[i][j];
+            }
+        }
+        return *this;
+    }
+
+    // Перегрузка оператора -=
+    Matrix& operator-=(const Matrix &other) {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                this->data[i][j] -= other.data[i][j];
+            }
+        }
+        return *this;
+    }
+
+    // Перегрузка оператора *= (умножение матрицы на число)
+    Matrix& operator*=(int scalar) {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                this->data[i][j] *= scalar;
+            }
+        }
+        return *this;
+    }
+
+    // Перегрузка оператора ввода >>
+    friend std::istream& operator>>(std::istream &is, Matrix &matrix) {
+        for (int i = 0; i < matrix.rows; ++i) {
+            for (int j = 0; j < matrix.cols; ++j) {
+                is >> matrix.data[i][j];
+            }
+        }
+        return is;
+    }
+
+    // Перегрузка оператора вывода <<
+    friend std::ostream& operator<<(std::ostream &os, const Matrix &matrix) {
+        for (int i = 0; i < matrix.rows; ++i) {
+            for (int j = 0; j < matrix.cols; ++j) {
+                os << matrix.data[i][j] << " ";
+            }
+            os << std::endl;
+        }
+        return os;
+    }
+};
 
 int main() {
-    Car car1;
-    Car car2("Toyota", 4, 150);
-    Lorry lorry1;
-    Lorry lorry2("Volvo", 6, 300, 5000);
+    Matrix mat1(2, 2);
+    Matrix mat2(2, 2);
 
-    std::cout << "Enter details for car1:\n";
-    std::cin >> car1;
-    std::cout << "car1: " << car1 << "\n";
+    std::cout << "Enter values for mat1 (2x2):" << std::endl;
+    std::cin >> mat1;
+    std::cout << "Enter values for mat2 (2x2):" << std::endl;
+    std::cin >> mat2;
 
-    std::cout << "car2: " << car2 << "\n";
+    Matrix sum = mat1 + mat2;
+    Matrix diff = mat1 - mat2;
+    Matrix product = mat1 * mat2;
 
-    std::cout << "Enter details for lorry1:\n";
-    std::cin >> lorry1;
-    std::cout << "lorry1: " << lorry1 << "\n";
+    mat1 += mat2;
+    mat1 -= mat2;
+    mat1 *= 2;
 
-    std::cout << "lorry2: " << lorry2 << "\n";
+    std::cout << "Sum of mat1 and mat2:\n" << sum;
+    std::cout << "Difference of mat1 and mat2:\n" << diff;
+    std::cout << "Product of mat1 and mat2:\n" << product;
 
-    std::cout << "Total cars and lorries created: " << Car::count() << "\n";
+    std::cout << "mat1 after += mat2:\n" << mat1;
+    std::cout << "mat1 after -= mat2:\n" << mat1;
+    std::cout << "mat1 after *= 2:\n" << mat1;
 
     return 0;
 }
